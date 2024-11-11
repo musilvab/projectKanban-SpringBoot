d1b4e1e2adaf9f3dfee3ca98aa1f571ed83b8824
Murilo Barbosa <91965497+musilvab@users.noreply.github.com>
2024-11-10 21:55:24 -0300

    Delete kanban directory

--- kanban/src/main/java/com/kanban/kanban/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kanban.kanban.repository;
-import com.kanban.kanban.model.TaskModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<TaskModel, Long> {
-
-}
